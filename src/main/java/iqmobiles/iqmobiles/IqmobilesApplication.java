74f78a8b9dfdcba42df57429aea368f6dd8b1667
root <root@ip-172-31-63-239.ec2.internal>
2023-09-05 03:19:46 +0000

    new store build

---
+++ src/main/java/iqmobiles/iqmobiles/IqmobilesApplication.java
@@ -0,0 +1,13 @@
+package iqmobiles.iqmobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqmobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqmobilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iqmobiles/iqmobiles/IqmobilesApplicationTests.java
@@ -0,0 +1,13 @@
+package iqmobiles.iqmobiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqmobilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
